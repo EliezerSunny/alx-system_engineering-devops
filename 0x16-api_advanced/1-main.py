b14865b8da69c97f2f67852b2efaf16a562d578a
EliezerSunny <adetunjieliazer@gmail.com>
2024-08-13 18:46:30 -0700

    modified

--- 0x16-api_advanced/1-main.py
+++ 0x16-api_advanced/1-main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+'''
+    this module contains the function top_ten
+'''
+import requests
+from sys import argv
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+
+def top_ten(subreddit):
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
