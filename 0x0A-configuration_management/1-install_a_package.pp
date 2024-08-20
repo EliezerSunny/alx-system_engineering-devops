d8a8ae50818b90e7f11cf62637e972c34fbf5484
EliezerSunny <adetunjieliazer@gmail.com>
2024-08-20 09:24:55 -0700

    modified

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# Ensure pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Ensure Flask 2.1.0 is installed using pip3
-package { 'Flask':
+# Install flask
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
